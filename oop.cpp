0edb6df3ab1bcff4a10b9ecfc4624eaa96557bbc
vladK <kryvenko.vlad@lll.kpi.ua>
2023-11-06 22:28:33 +0200

    додав привязки

--- oop.cpp
+++ oop.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
+#include <ctime>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(NULL));
+	int shipPos[12][24] = { 0 }; //двовимірний масив для прязки координат кораблів на мапі
+	int enemyShipPos[12][24] = { 0 }; //двумерный массив благодоря которому можно будет отслеживать:
 }
