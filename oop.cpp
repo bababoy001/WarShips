413b836186c36138164f9d6722f5f490293fb376
olegb <baranov.oleg@lll.kpi.ua>
2023-11-07 20:31:24 +0200

    реалізовані всі основні методі класу Ships для побудови за правилами і виводу всіх кораблів, також трохи перероблені printMap і Ship

--- oop.cpp
+++ oop.cpp
@@ -3,8 +3,11 @@
 #include <windows.h>
 #include <conio.h>
 #include <string>
+#include <vector>
 #define height 10
 #define length 10
+#define countShip 10
+
 using namespace std;
 
 
@@ -24,22 +27,137 @@ public:
 };
 
 
-class Ships {
+class Ship {
 public:
-	Ships(int numDeck, int typeShip, string nameCategoryShip, bool horizontal) {
+	Ship(int numDeck, int typeShip, string nameCategoryShip, bool horizontal) {
 		this->numDeck = numDeck;
 		this->typeShip = typeShip;
 		this->nameCategoryShip = nameCategoryShip;
 		this->horizontal = horizontal;
 	}
-private:
+
 	int numDeck;
 	int typeShip;
 	bool horizontal;
 	string nameCategoryShip;
-	int x;
-	int y;
+	vector<int> coorX;
+	vector<int> coorY;
 };
+
+
+
+class Ships {
+public:
+	Ships() {
+		
+	}
+	vector <Ship> allShips;
+	int countReadyShip = 0;
+	
+
+	void makeShip(Ship* ship_temp, Cell(*map)[length], bool random) {
+		if (random) {
+			randomPlaceShip(ship_temp, map);
+		}
+		if (!random) {
+			//PlaceShip(*ship_temp);
+		}
+
+	}
+
+	bool isCellForShip(Cell(*map)[length], int x, int y) {
+
+		if (!isCellInMap(x, y) || map[x][y].isShip)
+		{
+			return false;
+		}
+		for (int dx = -1; dx <= 1; dx++) {
+			for (int dy = -1; dy <= 1; dy++) {
+				if (map[x + dx][y + dy].isShip) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	void randomPlaceShip(Ship *ship_temp, Cell(*map)[length]) {
+		bool wrongPlace;
+		do {
+			wrongPlace = 0;
+			int x = rand() % 10;
+			int y = rand() % 10;
+			for (int i = 0; i < ship_temp->numDeck; i++) {
+				if (ship_temp->horizontal) {
+					if (!isCellForShip(map, x + i, y)) {
+						wrongPlace = 1;
+						break;
+					}
+					else {
+						ship_temp->coorX.push_back(x + i);
+						ship_temp->coorY.push_back(y);
+					}
+
+				}
+				else if (!ship_temp->horizontal) {
+					if (!isCellForShip(map, x, y + i)) {
+						wrongPlace = 1;
+						break;
+					}
+					else {
+						ship_temp->coorX.push_back(x);
+						ship_temp->coorY.push_back(y + i);
+					}
+				}
+			}
+			if (wrongPlace) {
+				ship_temp->horizontal = !ship_temp->horizontal;
+				ship_temp->coorX.clear();
+				ship_temp->coorY.clear();
+			}
+		} while (wrongPlace);
+
+		allShips.push_back(*ship_temp);
+		fromVectorToMap(ship_temp, map);
+		countReadyShip++;
+
+	}
+
+	bool isCellInMap(int x, int y) {
+		return(x >= 0) && (y >= 0) && (x < length) && (y < height);
+	}
+
+	void createFleet(Cell(*map)[length]) {
+		// Создание 1 четырехпалубного корабля
+		Ship fourDecker(4, 1, "четырехпалубный", rand() % 2);
+		makeShip(&fourDecker, map, true);
+
+		// Создание 2 трехпалубных кораблей
+		for (int i = 0; i < 2; ++i) {
+			Ship threeDecker(3, 1, "трехпалубный", rand() % 2);
+			makeShip(&threeDecker, map, true);
+		}
+
+		// Создание 3 двупалубных кораблей
+		for (int i = 0; i < 3; ++i) {
+			Ship twoDecker(2, 1, "двупалубный", rand() % 2);
+			makeShip(&twoDecker, map, true);
+		}
+
+		// Создание 4 однопалубных кораблей
+		for (int i = 0; i < 4; ++i) {
+			Ship oneDecker(1, 1, "однопалубный", rand() % 2);
+			makeShip(&oneDecker, map, true);
+		}
+	}
+private:
+	void fromVectorToMap(Ship* ship_temp, Cell(*map)[length]) {
+		for (int i = 0; i < ship_temp->numDeck; i++) {
+			map[ship_temp->coorX[i]][ship_temp->coorY[i]].isShip = 1;
+		}
+	}
+};
+
 
 void printMap(Cell (*map)[length]) {
 	cout << "   ";
@@ -49,6 +167,7 @@ void printMap(Cell (*map)[length]) {
 
 	cout << endl << "   _____________________" << endl;
 	cout << "  |                     |" << endl;
+
 	for (int i = 0; i < height; i++) {
 		
 		char letter = 'a' + i;
@@ -56,7 +175,13 @@ void printMap(Cell (*map)[length]) {
 		cout << letter << " | ";
 
 		for (int j = 0; j < length; j++) {
-			cout<< map[i][j].isShip<<" ";
+			if (map[i][j].isShip) {
+				cout << map[i][j].isShip << " ";
+			}
+			else {
+				cout << "  ";
+			}
+			
 		}
 
 		cout << "|" << endl;
@@ -64,14 +189,17 @@ void printMap(Cell (*map)[length]) {
 	cout << "  |_____________________|";
 }
 
+
+
 int main()
 {
 	srand(time(NULL));
 	Cell map[height][length]; //двовимірний масив для прязки координат союзних кораблів на мапі
 	Cell enemyMap[height][length]; //двовимірний масив для прязки координат ворожих кораблів на мапі
-	map[1][1].isShip = 1;
-	map[1][2].isShip = 1;
-	map[1][3].isShip = 1;
+	Ships myShips;
+	myShips.createFleet(map);
+	
+	
 	printMap(map);
 
 }
